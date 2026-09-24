b5d0e04d69430f1ccc4e4f3b05f3da0bd43bd4c8
Henrique <henrique11rodrigues@gmail.com>
2026-09-24 16:33:52 -0300

    Atualiza aplicativo e algoritmo genético

--- src/algoritmo.py
+++ src/algoritmo.py
@@ -2,26 +2,35 @@ from ports import *
 import copy
 import random
 
-nvar = 4
-portas = ['AND', 'OR', 'XOR', 'NAND']
-nos = 6
-nFilhos = 4
 
-minTermos = [0, 1, 3]
-
-IND_TAM = nos * 3 + 2
-IND_SAIDA = nos * 3
-IND_FIT = nos * 3 + 1
-
-SIM_NL = nvar + nos + 3
-SIM_NC = 2**nvar + 1
-SIM_LCOMP = SIM_NL - 1
-SIM_LSD = SIM_NL - 2
-SIM_LSE = SIM_NL - 3
+PORTAS = ['AND', 'OR', 'XOR', 'NAND']
 
 
-def criar_individuo():
-    ind = [0 for _ in range(IND_TAM)]
+def calcular_indices(nvar, nos):
+    ind_tam = nos * 3 + 2
+    ind_saida = nos * 3
+    ind_fit = nos * 3 + 1
+
+    sim_nl = nvar + nos + 3
+    sim_nc = 2**nvar + 1
+    sim_lcomp = sim_nl - 1
+    sim_lsd = sim_nl - 2
+    sim_lse = sim_nl - 3
+
+    return {
+        "IND_TAM": ind_tam,
+        "IND_SAIDA": ind_saida,
+        "IND_FIT": ind_fit,
+        "SIM_NL": sim_nl,
+        "SIM_NC": sim_nc,
+        "SIM_LCOMP": sim_lcomp,
+        "SIM_LSD": sim_lsd,
+        "SIM_LSE": sim_lse,
+    }
+
+
+def criar_individuo(nvar, nos, portas, indices):
+    ind = [0 for _ in range(indices["IND_TAM"])]
     cont = nvar - 1
 
     for i in range(2, nos * 3, 3):
@@ -30,45 +39,64 @@ def criar_individuo():
         ind[i] = portas[random.randint(0, len(portas) - 1)]
         cont += 1
 
-    ind[IND_SAIDA] = nos + nvar - 1
-    ind[IND_FIT] = -10
+    ind[indices["IND_SAIDA"]] = random.randint(nvar, nvar + nos - 1)
+    ind[indices["IND_FIT"]] = -10
     return ind
 
 
-def criar_tabela():
+def criar_tabela(nvar, nos, mintermos, indices):
     tabela = []
 
-    for i in range(SIM_NL):
-        linha = []
-        for j in range(SIM_NC):
-            linha.append(0)
+    for _ in range(indices["SIM_NL"]):
+        linha = [0 for _ in range(indices["SIM_NC"])]
         tabela.append(linha)
 
-    for i in range(SIM_NL - 3):
-        tabela[i][SIM_NC - 1] = i
+    for i in range(indices["SIM_NL"] - 3):
+        tabela[i][indices["SIM_NC"] - 1] = i
 
-    tabela[SIM_NL - 3][SIM_NC - 1] = 'SE'
-    tabela[SIM_NL - 2][SIM_NC - 1] = 'SD'
-    tabela[SIM_NL - 1][SIM_NC - 1] = 'CP'
+    tabela[indices["SIM_LSE"]][indices["SIM_NC"] - 1] = 'SE'
+    tabela[indices["SIM_LSD"]][indices["SIM_NC"] - 1] = 'SD'
+    tabela[indices["SIM_LCOMP"]][indices["SIM_NC"] - 1] = 'CP'
 
     for i in range(nvar):
         for j in range(2**i, 2**nvar, 2**(i + 1)):
             tabela[i][j:j + 2**i] = [1 for _ in range(2**i)]
 
-    for i in minTermos:
-        tabela[SIM_NL - 2][i] = 1
+    for i in mintermos:
+        if 0 <= i < 2**nvar:
+            tabela[indices["SIM_LSD"]][i] = 1
 
     return tabela
 
 
-def simula(ind, tabela):
+def ligantes(individuo, nvar, nos, indices):
+    vlig = [0 for _ in range(nos + nvar)]
+
+    vlig[individuo[indices["IND_SAIDA"]]] = individuo[indices["IND_SAIDA"]]
+
+    for i in range(individuo[indices["IND_SAIDA"]], nvar - 1, -1):
+        if vlig[i] != 0:
+            vlig[individuo[(i - nvar + 1) * 3 - 2]] = individuo[(i - nvar + 1) * 3 - 2]
+            vlig[individuo[(i - nvar + 1) * 3 - 3]] = individuo[(i - nvar + 1) * 3 - 3]
+
+    del vlig[:nvar]
+    vlig = [x for x in vlig if x != 0]
+
+    return vlig
+
+
+def custo(individuo, nvar, nos, indices):
+    return len(ligantes(individuo, nvar, nos, indices))
+
+
+def simula(individuo, tabela, nvar, nos, indices):
     t = copy.deepcopy(tabela)
-    ativos = ligantes(ind)
+    ativos = ligantes(individuo, nvar, nos, indices)
 
     for i in ativos:
-        p = ind[(i - nvar + 1) * 3 - 1]
-        ent1 = ind[(i - nvar + 1) * 3 - 2]
-        ent2 = ind[(i - nvar + 1) * 3 - 3]
+        p = individuo[(i - nvar + 1) * 3 - 1]
+        ent1 = individuo[(i - nvar + 1) * 3 - 2]
+        ent2 = individuo[(i - nvar + 1) * 3 - 3]
 
         if p == 'OR':
             t[i] = [p_OR(t[ent1][j], t[ent2][j]) for j in range(2**nvar)]
@@ -83,64 +111,89 @@ def simula(ind, tabela):
             t[i] = [p_NAND(t[ent1][j], t[ent2][j]) for j in range(2**nvar)]
             t[i].append(i)
 
-    t[SIM_LSE] = t[ind[IND_SAIDA]]
-    t[SIM_LCOMP] = [p_XNOR(t[SIM_LSD][i], t[SIM_LSE][i]) for i in range(2**nvar)]
-    t[SIM_LCOMP].append('CP')
+    t[indices["SIM_LSE"]] = t[individuo[indices["IND_SAIDA"]]]
+    t[indices["SIM_LCOMP"]] = [
+        p_XNOR(t[indices["SIM_LSD"]][i], t[indices["SIM_LSE"]][i])
+        for i in range(2**nvar)
+    ]
+    t[indices["SIM_LCOMP"]].append('CP')
 
-    res = [p_XNOR(t[SIM_LSD][i], t[SIM_LSE][i]) for i in range(2**nvar)]
+    res = [
+        p_XNOR(t[indices["SIM_LSD"]][i], t[indices["SIM_LSE"]][i])
+        for i in range(2**nvar)
+    ]
     pontuacao = sum(res)
 
-    return (pontuacao, t)
+    return pontuacao, t
 
 
-def ligantes(individuo):
-    vlig = [0 for _ in range(nos + nvar)]
-
-    vlig[individuo[IND_SAIDA]] = individuo[IND_SAIDA]
-
-    for i in range(individuo[IND_SAIDA], nvar - 1, -1):
-        if vlig[i] != 0:
-            vlig[individuo[(i - nvar + 1) * 3 - 2]] = individuo[(i - nvar + 1) * 3 - 2]
-            vlig[individuo[(i - nvar + 1) * 3 - 3]] = individuo[(i - nvar + 1) * 3 - 3]
-
-    del vlig[:nvar]
-    vlig = [x for x in vlig if x != 0]
-
-    return vlig
-
-
-def custo(individuo):
-    return len(ligantes(individuo))
-
-
-def mutacao(f, l, tabela):
-    filho = f[:]
-    ativos = l[:]
-    filho[IND_FIT] = -20
+def mutacao(filho_base, ativos, tabela, nvar, nos, portas, indices):
+    filho = filho_base[:]
+    filho[indices["IND_FIT"]] = -20
 
     while True:
-        gene = random.randint(0, IND_SAIDA - 1)
-        nos_mutado = gene // 3 + nvar
+        gene = random.randint(0, indices["IND_SAIDA"])
 
-        if (gene + 1) % 3 == 0:
-            gene_novo = portas[random.randint(0, len(portas) - 1)]
-            while gene_novo == filho[gene]:
-                gene_novo = portas[random.randint(0, len(portas) - 1)]
-            filho[gene] = gene_novo
-        else:
-            nova_conexao = random.randint(0, nos_mutado - 1)
-            while nova_conexao == filho[gene]:
-                nova_conexao = random.randint(0, nos_mutado - 1)
-            filho[gene] = nova_conexao
+        # Mutação da saída
+        if gene == indices["IND_SAIDA"]:
+            nova_saida = random.randint(
+                nvar,
+                nvar + nos - 1
+            )
 
-        if nos_mutado in ativos:
+            while nova_saida == filho[gene]:
+                nova_saida = random.randint(
+                    nvar,
+                    nvar + nos - 1
+                )
+
+            filho[gene] = nova_saida
             break
 
-    filho[IND_FIT] = simula(filho, tabela)[0]
+        # Mutação de um nó
+        no_mutado = gene // 3 + nvar
+
+        if (gene + 1) % 3 == 0:
+            gene_novo = portas[
+                random.randint(0, len(portas) - 1)
+            ]
+
+            while gene_novo == filho[gene]:
+                gene_novo = portas[
+                    random.randint(0, len(portas) - 1)
+                ]
+
+            filho[gene] = gene_novo
+
+        else:
+            nova_conexao = random.randint(
+                0,
+                no_mutado - 1
+            )
+
+            while nova_conexao == filho[gene]:
+                nova_conexao = random.randint(
+                    0,
+                    no_mutado - 1
+                )
+
+            filho[gene] = nova_conexao
+
+        if no_mutado in ativos:
+            break
+
+    filho[indices["IND_FIT"]] = simula(
+        filho,
+        tabela,
+        nvar,
+        nos,
+        indices
+    )[0]
+
     return filho
 
 
-def construir_expressao(individuo, no):
+def construir_expressao(individuo, no, nvar):
     if no < nvar:
         return chr(ord('A') + no)
 
@@ -149,8 +202,8 @@ def construir_expressao(individuo, no):
     ent2 = individuo[base + 1]
     porta = individuo[base + 2]
 
-    e1 = construir_expressao(individuo, ent1)
-    e2 = construir_expressao(individuo, ent2)
+    e1 = construir_expressao(individuo, ent1, nvar)
+    e2 = construir_expressao(individuo, ent2, nvar)
 
     if porta == 'AND':
         return f"({e1} . {e2})"
@@ -164,20 +217,19 @@ def construir_expressao(individuo, no):
     return f"({e1} {porta} {e2})"
 
 
-def executar_algoritmo(callback_progresso=None):
+def executar_algoritmo(nvar, nos, max_geracoes, n_filhos, mintermos, callback_progresso=None):
 
-    ind = criar_individuo()
-    tabela = criar_tabela()
+    indices = calcular_indices(nvar, nos)
+    individuo_inicial = criar_individuo(nvar, nos, PORTAS, indices)
+    tabela = criar_tabela(nvar, nos, mintermos, indices)
 
-    pai = ind[:]
-    pai[IND_FIT] = simula(pai, tabela)[0]
+    pai = individuo_inicial[:]
+    pai[indices["IND_FIT"]] = simula(pai, tabela, nvar, nos, indices)[0]
 
     melhor = pai[:]
     melhor_perfeito = None
 
     geracao = 0
-    max_geracoes = 50000
-    n_filhos = 20
 
     while geracao < max_geracoes:
         geracao += 1
@@ -185,38 +237,43 @@ def executar_algoritmo(callback_progresso=None):
         if callback_progresso is not None and (geracao % 100 == 0 or geracao == max_geracoes):
             callback_progresso(geracao, max_geracoes)
 
-        ativos = ligantes(pai)
-
+        ativos = ligantes(pai, nvar, nos, indices)
         melhor_filho = None
 
         for _ in range(n_filhos):
-            filho = mutacao(pai, ativos, tabela)
+            filho = mutacao(pai, ativos, tabela, nvar, nos, PORTAS, indices)
 
             if melhor_filho is None:
                 melhor_filho = filho[:]
                 continue
 
             if melhor_perfeito is None:
-                if filho[IND_FIT] > melhor_filho[IND_FIT]:
+                if filho[indices["IND_FIT"]] > melhor_filho[indices["IND_FIT"]]:
                     melhor_filho = filho[:]
             else:
-                if filho[IND_FIT] == 2**nvar and custo(filho) < custo(melhor_filho):
+                if (
+                    filho[indices["IND_FIT"]] == 2**nvar
+                    and custo(filho, nvar, nos, indices) < custo(melhor_filho, nvar, nos, indices)
+                ):
                     melhor_filho = filho[:]
 
         if melhor_perfeito is None:
-            if melhor_filho[IND_FIT] >= pai[IND_FIT]:
+            if melhor_filho[indices["IND_FIT"]] >= pai[indices["IND_FIT"]]:
                 pai = melhor_filho[:]
         else:
-            if melhor_filho[IND_FIT] == 2**nvar and custo(melhor_filho) <= custo(pai):
+            if (
+                melhor_filho[indices["IND_FIT"]] == 2**nvar
+                and custo(melhor_filho, nvar, nos, indices) <= custo(pai, nvar, nos, indices)
+            ):
                 pai = melhor_filho[:]
 
-        if pai[IND_FIT] > melhor[IND_FIT]:
+        if pai[indices["IND_FIT"]] > melhor[indices["IND_FIT"]]:
             melhor = pai[:]
 
-        if pai[IND_FIT] == 2**nvar:
+        if pai[indices["IND_FIT"]] == 2**nvar:
             if melhor_perfeito is None:
                 melhor_perfeito = pai[:]
-            elif custo(pai) < custo(melhor_perfeito):
+            elif custo(pai, nvar, nos, indices) < custo(melhor_perfeito, nvar, nos, indices):
                 melhor_perfeito = pai[:]
 
     if melhor_perfeito is not None:
@@ -226,9 +283,9 @@ def executar_algoritmo(callback_progresso=None):
         individuo_final = melhor
         mensagem = "Nenhuma solução perfeita encontrada. Melhor indivíduo parcial."
 
-    fitness = individuo_final[IND_FIT]
-    nos_ativos = custo(individuo_final)
-    expressao = construir_expressao(individuo_final, individuo_final[IND_SAIDA])
+    fitness = individuo_final[indices["IND_FIT"]]
+    nos_ativos = custo(individuo_final, nvar, nos, indices)
+    expressao = construir_expressao(individuo_final, individuo_final[indices["IND_SAIDA"]], nvar)
 
     return {
         "mensagem": mensagem,
@@ -241,7 +298,14 @@ def executar_algoritmo(callback_progresso=None):
 
 
 if __name__ == "__main__":
-    resultado = executar_algoritmo()
+    resultado = executar_algoritmo(
+        nvar=3,
+        nos=6,
+        max_geracoes=50000,
+        n_filhos=4,
+        mintermos=[0, 1, 3]
+    )
+
     print("Gerações:", resultado["geracoes"])
     print(resultado["mensagem"])
     print("Fitness:", resultado["fitness"])
--- src/main.py
+++ src/main.py
@@ -1,46 +1,135 @@
 import os
-import time
 import threading
 
-inicio = time.time()
-print("1. Início do arquivo")
+__version__ = "0.1.0"
 
 os.environ["KIVY_GL_BACKEND"] = "sdl2"
 
+from kivy.utils import platform
+
+if platform != "android":
+    from kivy.config import Config
+    Config.set("input", "mouse", "mouse,multitouch_on_demand")
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 from kivy.properties import StringProperty, NumericProperty, BooleanProperty
 from kivy.lang import Builder
 from kivy.clock import Clock
+
 from algoritmo import executar_algoritmo
+
 
 Builder.load_file("app.kv")
 
 
+class ResultadoPopup(BoxLayout):
+    progresso = NumericProperty(0)
+    progresso_texto = StringProperty("Preparando execução...")
+    resultado = StringProperty("")
+    executando = BooleanProperty(True)
+
+    def fechar(self):
+        if hasattr(self, "popup"):
+            self.popup.dismiss()
+
 
 class TelaPrincipal(BoxLayout):
-    resultado = StringProperty(
-        "Clique em 'Executar algoritmo' para gerar um circuito lógico."
-    )
-    progresso = NumericProperty(0)
-    progresso_texto = StringProperty("0%")
     executando = BooleanProperty(False)
 
     def executar(self):
         if self.executando:
             return
 
-        self.executando = True
-        self.progresso = 0
-        self.progresso_texto = "0%"
-        self.resultado = "Executando algoritmo...\nAguarde..."
-
-        thread = threading.Thread(target=self.executar_em_background, daemon=True)
-        thread.start()
-
-    def executar_em_background(self):
         try:
-            res = executar_algoritmo(callback_progresso=self.receber_progresso)
+            # Lê e valida os parâmetros
+            nvar = int(self.ids.input_nvar.text)
+            nos = int(self.ids.input_nos.text)
+            max_geracoes = int(self.ids.input_geracoes.text)
+            n_filhos = int(self.ids.input_filhos.text)
+
+            mintermos = self.parse_mintermos(
+                self.ids.input_mintermos.text
+            )
+
+            self.executando = True
+
+            # Cria o conteúdo do modal
+            self.modal_resultado = ResultadoPopup()
+
+            # Cria o Popup
+            self.popup_resultado = Popup(
+                title="",
+                content=self.modal_resultado,
+                size_hint=(0.9, 0.85),
+                auto_dismiss=False
+            )
+
+            # Permite que ResultadoPopup feche seu próprio Popup
+            self.modal_resultado.popup = self.popup_resultado
+
+            # Estado inicial
+            self.modal_resultado.executando = True
+            self.modal_resultado.progresso = 0
+            self.modal_resultado.progresso_texto = "0%"
+            self.modal_resultado.resultado = (
+                "Executando algoritmo...\n"
+                "Aguarde..."
+            )
+
+            # Abre o modal
+            self.popup_resultado.open()
+
+            # Executa o algoritmo fora da thread da interface
+            thread = threading.Thread(
+                target=self.executar_em_background,
+                args=(
+                    nvar,
+                    nos,
+                    max_geracoes,
+                    n_filhos,
+                    mintermos
+                ),
+                daemon=True
+            )
+
+            thread.start()
+
+        except ValueError as e:
+            self.exibir_erro(str(e))
+
+    def parse_mintermos(self, texto):
+        texto = texto.strip()
+
+        if not texto:
+            return []
+
+        partes = texto.split(",")
+
+        return [
+            int(p.strip())
+            for p in partes
+            if p.strip() != ""
+        ]
+
+    def executar_em_background(
+        self,
+        nvar,
+        nos,
+        max_geracoes,
+        n_filhos,
+        mintermos
+    ):
+        try:
+            res = executar_algoritmo(
+                nvar=nvar,
+                nos=nos,
+                max_geracoes=max_geracoes,
+                n_filhos=n_filhos,
+                mintermos=mintermos,
+                callback_progresso=self.receber_progresso
+            )
 
             texto = (
                 f"{res['mensagem']}\n"
@@ -54,28 +143,72 @@ class TelaPrincipal(BoxLayout):
                 f"{res['individuo']}"
             )
 
-            Clock.schedule_once(lambda dt: self.finalizar_execucao(texto))
-
-        except Exception as e:
             Clock.schedule_once(
-                lambda dt: self.finalizar_execucao(f"Erro ao executar:\n{str(e)}")
+                lambda dt: self.finalizar_execucao(texto)
             )
 
-    def receber_progresso(self, geracao_atual, total_geracoes):
-        percentual = int((geracao_atual / total_geracoes) * 100)
+        except Exception as e:
+            erro = str(e)
+
+            Clock.schedule_once(
+                lambda dt: self.finalizar_execucao(
+                    f"Erro ao executar:\n{erro}",
+                    erro=True
+                )
+            )
+
+    def receber_progresso(
+        self,
+        geracao_atual,
+        total_geracoes
+    ):
+        percentual = int(
+            (geracao_atual / total_geracoes) * 100
+        )
+
         Clock.schedule_once(
             lambda dt: self.atualizar_progresso(percentual)
         )
 
     def atualizar_progresso(self, percentual):
-        self.progresso = percentual
-        self.progresso_texto = f"{percentual}%"
+        if hasattr(self, "modal_resultado"):
+            self.modal_resultado.progresso = percentual
+            self.modal_resultado.progresso_texto = (
+                f"{percentual}%"
+            )
 
-    def finalizar_execucao(self, texto):
-        self.progresso = 100
-        self.progresso_texto = "100%"
-        self.resultado = texto
+    def finalizar_execucao(self, texto, erro=False):
+        if hasattr(self, "modal_resultado"):
+
+            if erro:
+                self.modal_resultado.progresso_texto = "Erro"
+            else:
+                self.modal_resultado.progresso = 100
+                self.modal_resultado.progresso_texto = "100%"
+
+            self.modal_resultado.resultado = texto
+            self.modal_resultado.executando = False
+
         self.executando = False
+
+    def exibir_erro(self, mensagem):
+        conteudo = ResultadoPopup()
+
+        popup = Popup(
+            title="",
+            content=conteudo,
+            size_hint=(0.9, 0.6),
+            auto_dismiss=False
+        )
+
+        conteudo.popup = popup
+        conteudo.executando = False
+        conteudo.progresso = 0
+        conteudo.progresso_texto = "Erro nos parâmetros"
+        conteudo.resultado = mensagem
+
+        popup.open()
+
 
 class MeuApp(App):
     def build(self):
@@ -84,4 +217,3 @@ class MeuApp(App):
 
 if __name__ == "__main__":
     MeuApp().run()
-    print("11. Depois do run:", time.time() - inicio)
